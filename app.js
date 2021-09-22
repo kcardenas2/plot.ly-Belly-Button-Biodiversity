9600fa19b92c55f76c573370c08e18af963d162b
Kimberly Cardenas <you@example.com>
2021-09-22 14:05:33 -0700

    bar chart code

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ function DifferentCharts(samples){
         var selectsample =data.selectsample;
         var resultsarray = metadata.filter(sampleobject =>
             sampleobject.id == sample);
-    }   var results = resultsarray[0]
+       
+        var results = resultsarray[0]
         var id = results.otu_ids;
         var values = results.sample_values;
         var labels = results.otu_lables;
@@ -31,18 +32,37 @@ function DifferentCharts(samples){
         var layout ={
             xaxis:{ title: "OTU ID"}
         };
-        var trace1=[
-            x: id,
-            y: values,
-            mode:"markers",
-            text: lables,
-            marker:{
-                color: id,
-                size: values,
+            var trace1=[
+                y= values,
+                x= id,
+                mode="markers",
+                text= lables,
+                marker={
+                    color: id,
+                    size: values,
             }
         ];
         console.log(values);
         console.log(id);
         console.log(values);
         Plotly.newplot("bubble", trace1, layout);
+
+
+// bar chart
+        var trace2=[{
+            y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            x:values.slice(0,10).reverse(),
+            text:labels.slice(0,10).reverse(),
+            type:"bar",
+            orientation:"h"
+
+  
+        }
+
+        ];
+        var barlayout={
+            title: "Top 10 Bacteria"
+        };
+        Plotly.newplot("bar", trace2, barlayout);
+        });
 }
