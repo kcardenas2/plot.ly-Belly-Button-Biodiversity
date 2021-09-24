e317e3b9da6d468e99236c3184853eabb72bd7e8
Kimberly Cardenas <you@example.com>
2021-09-23 17:56:35 -0700

    fixed problems

--- app.js
+++ app.js
@@ -5,26 +5,26 @@ function metadata(sample){
             sampleobject.id == sample);
         var results = resultsarray[0]
         var samples = d3.select("#sample-metadata");
-        sample.html("");
+        samples.html("");
 
-        Object.entries(result).forEach(([a,b]) =>{
+        Object.entries(results).forEach(([a,b]) =>{
             samples.append("h5").text(`${a}: ${b}`);
         });
     });
 }
-function DifferentCharts(samples){
+function DifferentCharts(sample){
     d3.json("samples.json").then((data)=>{
-        var selectsample =data.selectsample;
-        var resultsarray = metadata.filter(sampleobject =>
+        var samples =data.samples;
+        var resultsarray = samples.filter(sampleobject =>
             sampleobject.id == sample);
        
         var results = resultsarray[0]
         var id = results.otu_ids;
         var values = results.sample_values;
-        var labels = results.otu_lables;
+        var labels = results.otu_labels;
         
 
-        console.log(selectsample);
+        console.log(samples);
         console.log(resultsarray);
         console.log(results);
         
@@ -32,25 +32,27 @@ function DifferentCharts(samples){
         var layout ={
             xaxis:{ title: "OTU ID"}
         };
-            var trace1=[
-                y= values,
-                x= id,
-                mode="markers",
-                text= lables,
-                marker={
+            var trace1 = [
+                {
+                y: values,
+                x:id,
+                mode:"markers",
+                text: labels,
+                marker:{
                     color: id,
                     size: values,
+                }
             }
         ];
         console.log(values);
         console.log(id);
         console.log(values);
-        Plotly.newplot("bubble", trace1, layout);
+        Plotly.plot("bubble", trace1, layout);
 
 
 // bar chart
         var trace2=[{
-            y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+            y:id.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
             x:values.slice(0,10).reverse(),
             text:labels.slice(0,10).reverse(),
             type:"bar",
@@ -63,6 +65,28 @@ function DifferentCharts(samples){
         var barlayout={
             title: "Top 10 Bacteria"
         };
-        Plotly.newplot("bar", trace2, barlayout);
+        Plotly.newPlot("bar", trace2, barlayout);
         });
 }
+
+function init(){
+    var dropdown =d3.select("#selDataset");
+    d3.json("samples.json").then((data)=>{
+        var names =data.names;
+        names.forEach((sample) =>{
+        dropdown
+        .append("option")
+        .text(sample)
+    });
+    const Sample=names[0];
+    DifferentCharts(Sample);
+    metadata(Sample);
+    });
+}
+function optionChanged(NewSample){
+    DifferentCharts(NewSample);
+    metadata(NewSample);
+}
+
+
+init();
